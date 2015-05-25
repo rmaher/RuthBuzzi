bfa48bbf4f38add9bb966c1fca6f38b01d4dc539
Rachel Maher <rachelelizabethmaher@gmail.com>
2015-05-24 22:04:47 -0600

    refactoring of scope

--- tipcalc/tipcalc.js
+++ tipcalc/tipcalc.js
@@ -1,38 +1,25 @@
 angular.module('tipApp', [])
     .controller('MyCtrl', ['$scope', function($scope) {
+       $scope.submitForm = function() {
 
-       $scope.basemeal = "";
-       $scope.taxrate = "";
-       $scope.tippercent = "";
-       $scope.earnings = "";
-       $scope.tipeval = function() {
-          $scope.tiptotal = $scope.basemeal * $scope.tippercent/100;
+             $scope.tiptotal = $scope.basemeal * $scope.tippercent/100;
+
+             $scope.taxtotal = $scope.basemeal * $scope.taxrate/100 + $scope.basemeal;
+
+             $scope.mealtotal = $scope.taxtotal + $scope.tiptotal;
+
+             $scope.fulltiptotal = $scope.fulltiptotal + $scope.tiptotal;
+
+             $scope.mealcount = $scope.mealcount + 1;
+
+             $scope.avgtip = $scope.fulltiptotal/$scope.mealcount;
+
        };
-       $scope.calctax = function() {
-          $scope.taxtotal = $scope.basemeal * $scope.taxrate/100 + $scope.basemeal;
-       };
-       $scope.calctotal = function() {
-          $scope.mealtotal = $scope.taxtotal + $scope.tiptotal;
-       };
+
        $scope.clearfields = function() {
-          $scope.basemeal = "";
-          $scope.taxrate = "";
-          $scope.tippercent = "";
-       };
-
-       $scope.grandtiptotal = function() {
-          $scope.fulltiptotalarr = [];
-          for(i = 0; i < $scope.tipeval; i++){
-            $scope.tiptotal.push($scope.fulltiptotalarr);
-          };
-          $scope.fulltiptotal=0;
-          $.each(fulltiptotalarr,function() {
-            $scope.fulltiptotal += this;
-          });
-       };
-
-       $scope.averagetip = function () {
-          $scope.avgtip = $scope.fulltiptotal/$scope.mealcount;
+                $scope.basemeal = "";
+                $scope.taxrate = "";
+                $scope.tippercent = "";
        };
 
     }]); 
