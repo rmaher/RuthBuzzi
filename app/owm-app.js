65eb68008927fd315871f63dae5e7834e045a16e
Rachel Maher <rachelelizabethmaher@gmail.com>
2015-06-06 13:19:59 -0600

    working animations

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,32 +1,45 @@
-angular.module('OWMApp', ['ngRoute'])
-    .value('owmCities', ['New York', 'Dallas', 'Chicago'])
+angular.module('OWMApp', ['ngRoute', 'ngAnimate'])
+    .value('owmCities', ['New_York', 'Dallas', 'Chicago'])
     .config(function($routeProvider) {
         $routeProvider.when('/', {
-            templateUrl : './home.html',
+            templateUrl : 'home.html',
             controller : 'HomeCtrl'
         })
         .when('/cities/:city', {
-            templateUrl : './city.html',
+            templateUrl : 'city.html',
             controller : 'CityCtrl',
             resolve : {
-            	city: function(owmCities, $route, $location) {
-            		var city = $route.current.params.city;
-            		if(owmCities.indexOf(city) == -1 ) {
-            			$location.path('/error');
-            			return;
-            		}
-            		return city;
-            	}
+                city: function(owmCities, $route, $location) {
+                    var city = $route.current.params.city;
+                    if(owmCities.indexOf(city) == -1 ) {
+                        $location.path('/error');
+                        return;
+                    }
+                    return city;
+                }
             }
         })
         .when('/error', {
-        	template : '<p>Error - Page Not Found</p>'
+            template : '<p>Error - Page Not Found</p>'
         });
     
     })
-    .controller('HomeCtrl', ['$scope', function($scope) {
+    .run(function($rootScope, $location, $timeout) {
+    $rootScope.$on('$routeChangeError', function() {
+        $location.path("/error");
+    });
+    $rootScope.$on('$routeChangeStart', function() {
+        $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function() {
+      $timeout(function() {
+        $rootScope.isLoading = false;
+      }, 1000);
+    });
+    })
+    .controller('HomeCtrl', function($scope) {
         //empty for now
     })
-    .controller('CityCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-    	$scope.city = city;
+    .controller('CityCtrl', function($scope, $routeParams, city) {
+        $scope.city = city;
     });
